53302b2df8b5a65925c77d88c75c218497f02257
He Guimin <guimin.hgm@alibaba-inc.com>
2020-02-24 13:47:28 +0800

    add a new dnat module

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "alicloud" {
+  profile                 = var.profile != "" ? var.profile : null
+  shared_credentials_file = var.shared_credentials_file != "" ? var.shared_credentials_file : null
+  region                  = var.region != "" ? var.region : null
+  skip_region_validation  = var.skip_region_validation
+  configuration_source    = "terraform-alicloud-modules/dnat"
+}
+
+locals {
+  dnat_table_id = var.dnat_table_id != "" ? var.dnat_table_id : var.nat_gateway_id != "" ? concat(data.alicloud_nat_gateways.this.gateways.*.dnat_table_id, [""])[0] : ""
+}
+
+data "alicloud_nat_gateways" "this" {
+  ids = var.nat_gateway_id != "" ? [var.nat_gateway_id] : null
+}
+
+resource "alicloud_forward_entry" "this" {
+  count = var.create ? length(var.entries) : 0
+
+  forward_table_id = local.dnat_table_id
+
+  name = lookup(var.entries[count.index], "name", format("tf-dnat-entry%3d", count.index + 1))
+
+  ip_protocol = lookup(var.entries[count.index], "ip_protocol", "any")
+
+  external_ip   = lookup(var.entries[count.index], "external_ip", var.external_ip)
+  external_port = lookup(var.entries[count.index], "external_port", "any")
+
+  internal_ip = lookup(var.entries[count.index], "internal_ip", )
+
+  // Default to external_port
+  internal_port = lookup(var.entries[count.index], "internal_port", lookup(var.entries[count.index], "external_port", "any"))
+}
+
+
---
+++ variables.tf
@@ -0,0 +1,59 @@
+#################
+# Provider
+#################
+variable "region" {
+  description = "The region used to launch this module resources."
+  type        = string
+  default     = ""
+}
+
+variable "profile" {
+  description = "The profile name as set in the shared credentials file. If not set, it will be sourced from the ALICLOUD_PROFILE environment variable."
+  type        = string
+  default     = ""
+}
+variable "shared_credentials_file" {
+  description = "This is the path to the shared credentials file. If this is not set and a profile is specified, $HOME/.aliyun/config.json will be used."
+  type        = string
+  default     = ""
+}
+
+variable "skip_region_validation" {
+  description = "Skip static validation of region ID. Used by users of alternative AlibabaCloud-like APIs or users w/ access to regions that are not public (yet)."
+  type        = bool
+  default     = false
+}
+
+#################
+# Dnat Entries
+#################
+variable "create" {
+  description = "Whether to create dnat entries. If true, the 'entries' should be set."
+  type        = bool
+  default     = true
+}
+
+variable "nat_gateway_id" {
+  description = "The id of a nat gateway used to fetch the 'dnat_table_id'."
+  type        = string
+  default     = ""
+}
+
+variable "dnat_table_id" {
+  description = "The dnat table id to use on all dnat entries."
+  type        = string
+  default     = ""
+}
+
+variable "entries" {
+  description = "A list of entries to create. Each item valid keys: 'name'(default to a string with prefix 'tf-dnat-entry' and numerical suffix), 'ip_protocol'(default to 'any'), 'external_ip'(if not, use root parameter 'external_ip'), 'external_port'(default to 'any'), 'internal_ip'(required), 'internal_port'(default to the 'external_port')."
+  type        = list(map(string))
+  default     = []
+}
+
+variable "external_ip" {
+  description = "The public ip address to use on all dnat entries."
+  type        = string
+  default     = ""
+}
+
